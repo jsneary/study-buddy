681e22a39caa8075f96f71bf2490aa5835788f4e
Justus Sneary <jsneary@eagles.bridgewater.edu>
2024-02-14 13:35:56 -0500

    committing changes

---
+++ js/signup.js
@@ -0,0 +1,33 @@
+let createUser = async function() {
+    console.log("running create user")
+
+    const url = "https://studybuddy-api.azurewebsites.net/user"
+
+    const user = {
+        "email": "jsneary6@eagles.bridgewater.edu",
+        "username": "justus123",
+        "school": "Bridgewater",
+        "majors": [],
+        "_id": "65ccdcb28d5bc75d3b5795b6"
+    }
+
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user)
+    }
+
+    let response = await fetch(url, options)
+
+    if (response.status == 200) {
+
+        console.log("User created")
+
+        
+    }
+    else {
+
+        console.log("request error")
+
+    }
+}
